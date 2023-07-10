4ebd2d5eaf6af4c17c85b5c8d2f8789e75a7a72d
Diego-yednak <58500420+Diego-yednak@users.noreply.github.com>
2023-07-09 23:00:41 -0300

    WIP

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MySelectComponent } from './my-select-custom/my-select-custom.component';
 
 @NgModule({
-  imports: [ BrowserModule, BrowserAnimationsModule ,ReactiveFormsModule, MatFormFieldModule, MatSelectModule ],
-  declarations: [ AppComponent, MySelectComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+  ],
+  declarations: [AppComponent, MySelectComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
