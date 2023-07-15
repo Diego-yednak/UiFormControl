15bcf69679a73babf27dcbceaaf6abe56219af45
Diego-yednak <58500420+Diego-yednak@users.noreply.github.com>
2023-07-15 18:25:33 -0300

    1 file modified

--- src/app/my-select-custom/my-select-custom.component.ts
+++ src/app/my-select-custom/my-select-custom.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
-    selector: 'my-select',
-    template: `
+  selector: 'my-select',
+  template: `
   <mat-form-field>
     <mat-select>
       <mat-option *ngFor = "let option of cities" [value]="option">
@@ -12,12 +12,12 @@ import { FormControl } from '@angular/forms';
     </mat-select>
   </mat-form-field>
   `,
-    styles: [`:host { display: block; }`],
+  styles: [`:host { display: block; }`],
 })
 export class MySelectComponent {
-    @Input() ctrl: FormControl;
-    // @Input() options: string[];
-    // @Input() placeholder: string;
-    cities = ['Krakow', 'Warszawa', 'Gdansk'];
-    countries = ['Poland', 'Germany', 'Sweden'];
+  @Input() ctrl: FormControl;
+  // @Input() options: string[];
+  // @Input() placeholder: string;
+  cities = ['Krakow', 'Warszawa', 'Gdansk'];
+  countries = ['Poland', 'Germany', 'Sweden'];
 }
