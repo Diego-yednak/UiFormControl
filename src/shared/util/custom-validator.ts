eb708ac2d5776df826dd4fa16432302a9c4e23e9
Diego <diego.yednak@telefonica.com>
2023-07-16 13:55:29 -0300

    fix type return validator

--- src/shared/util/custom-validator.ts
+++ src/shared/util/custom-validator.ts
@@ -1,22 +1,23 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 import { ErrorMessageControl } from './error-message-control';
 import { UiRegexUtil } from './ui-regex-util';
+import { ErrorMessageModel } from '../model/error-message.model';
 
 export class CustomValidator {
-    static required(control: AbstractControl): ValidationErrors | null {
+    static required(control: AbstractControl): ErrorMessageModel | null {
         if (!control.value) {
             return ErrorMessageControl.required
         }
         return null;
     }
-    static requiredNumber(control: AbstractControl): ValidationErrors | null {
+    static requiredNumber(control: AbstractControl): ErrorMessageModel | null {
         const value = control.value as number;
         if (value && isNaN(Number(value))) {
             return ErrorMessageControl.requiredNumber
         }
         return null;
     }
-    static email(control: AbstractControl): ValidationErrors | null {
+    static email(control: AbstractControl): ErrorMessageModel | null {
         const value = control.value as string;
         if (value && !UiRegexUtil.email(value)) {
             return ErrorMessageControl.email
